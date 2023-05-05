29beffb045ee767e66b98cdc2aa208803f04f516
Pierre-Marie Marchio <pierremariemarchio@gmail.com>
2023-05-05 14:07:14 +0200

    suite

--- Js/index.js
+++ Js/index.js
@@ -16,7 +16,25 @@ document.getElementById("title").innerHTML = "Good By!";
 
 document.querySelector("h1").style.color = "white";
 document.querySelector("#title").style.background = "black";
-document.querySelector("#list a").style.color = "green"; 
+document.querySelector("#list a").style.color = "green";
 
 document.querySelectorAll("#list .item")[1].style.color = "blue";
 document.querySelectorAll("#list .item")[2].style.color = "blue";
+document.querySelector("body .btn").style.backgroundColor = "yellow";
+
+// DOM classes manipulation
+
+function changeSizeH1() {
+  document.querySelector("h1").classList.toggle("bigHeading");
+}
+
+// DOM Atribute 
+
+function changehref() {
+
+  if (document.querySelector("input").checked == true) {
+    document.querySelector("#list a").setAttribute("href", "https://www.google.com");
+  } else {
+    document.querySelector("#list a").setAttribute("href", "https://www.bing.com");
+  }
+}
